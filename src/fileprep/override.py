fb87e9635a1a79908646e2d71ffa0b82ec565b9c
jbernardis <jeff.bernardis@gmail.com>
2014-05-10 17:58:34 -0400

    Initial testing of slicer over-rides

--- src/fileprep/override.py
+++ src/fileprep/override.py
@@ -3,16 +3,41 @@ import wx
 class Override(wx.Panel):
 	def __init__(self, parent):
 		wx.Panel.__init__(self, parent, -1)
+		self.SetBackgroundColour("white")
 
 		box = wx.StaticBox(self, -1, "Slicer Overrides:")
+		box.SetBackgroundColour("white")
 		bsizer = wx.StaticBoxSizer(box, wx.VERTICAL)
 
-		t = wx.StaticText(self, -1, "Controls placed \"inside\" the box are really its siblings")
-		bsizer.Add(t, 0, wx.TOP|wx.LEFT, 10)
+		bgrid = wx.GridBagSizer()
+		bgrid.AddSpacer((10, 10), pos=(0,0))
+		bgrid.AddSpacer((10, 10), pos=(0,2))
+		bgrid.AddSpacer((10, 10), pos=(0,4))
 
+		self.cbOvLH = wx.CheckBox(self, wx.ID_ANY, "Layer Height")
+		self.Bind(wx.EVT_CHECKBOX, self.checkLH, self.cbOvLH)
+		bgrid.Add(self.cbOvLH, pos=(1, 1))
+		self.teOvLH = wx.TextCtrl(self, wx.ID_ANY, "0.2", style=wx.TE_RIGHT)
+		self.teOvLH.Enable(False)
+		bgrid.AddSpacer((10, 10), pos=(2,0))
+
+		bgrid.Add(self.teOvLH, pos=(1,3))
+
+
+		bsizer.Add(bgrid)
 		border = wx.BoxSizer()
-		border.Add(bsizer, 1, wx.EXPAND|wx.ALL, 25)
+		border.Add(bsizer, 1, wx.EXPAND|wx.ALL, 10)
 		self.SetSizer(border)   
+	def checkLH(self, evt):
+		if self.cbOvLH.IsChecked():
+			self.teOvLH.Enable(True)
+		else:
+			self.teOvLH.Enable(False)
 		
 	def getOverrides(self):
-		return {}     
+		r = {}     
+		if self.cbOvLH.IsChecked():
+			r['layerheight'] = self.teOvLH.GetValue()
+
+		print "get ovr returning ", r
+		return r
